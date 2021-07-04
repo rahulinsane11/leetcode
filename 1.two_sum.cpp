d6fdd7ebadf6f0ff79bda813d249b4041bd735ca
rahulinsane11 <52071056+rahulinsane11@users.noreply.github.com>
2021-07-05 02:41:41 +0530

    Add files via upload

---
+++ 1.two_sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(nums.at(i) + nums.at(j) == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+                    
+            }
+        }
+        return result;
+    }
+};
